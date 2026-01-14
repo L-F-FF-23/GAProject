83cc5b8cd52ce019873d336291d12ce04821f213
LuddeFlor <lucasflorentin2007@gmail.com>
2026-01-14 17:28:51 +0100

    real update

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import torch
 from sklearn.model_selection import train_test_split
 from torch import nn
-import json
-import csv
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
+from pathlib import Path
+from torch.utils.data import DataLoader, TensorDataset
+from collections import defaultdict
 
 torch.manual_seed(42)
 
@@ -13,65 +15,81 @@ encoder = OneHotEncoder()
 
 fig, ax = plt.subplots(1, 3, figsize=(15, 5))
 
-with open("data.json", "r") as f:
-    data = json.load(f)
-
 allteams = []
 
-for match in data:
-    allteams.append(data[match]["Team1"])
-    allteams.append(data[match]["Team2"])
+with open(file="2025_LoL_esports_match_data_from_OraclesElixir.csv", mode="r", newline="", encoding="utf-8") as f:
+    data = csv.DictReader(f)
+    datalist = list(data)
 
+h2h = defaultdict(lambda: [0, 0])
+
+for row in datalist:
+    if row["participantid"] == "100" or row["participantid"] == "200":
+        allteams.append(row["teamname"])
+
+matchCount = len(allteams)//2
 finishedteams = np.unique(np.array(allteams)).reshape(-1,1)
-
-teamsdata = torch.zeros(len(data), 6)
-resultsdata = torch.zeros(len(data), 1)
+twoteamfeats = len(finishedteams)*2
+teamsdata = torch.zeros(matchCount, twoteamfeats)
+resultsdata = torch.zeros(matchCount, 1)
+winrates = np.zeros(shape=(445,443))
 
 encoder.fit(finishedteams)
 
 loopnum = 0
 
-for match in data:
-    x = (encoder.transform(np.array([data[match]["Team1"]]).reshape(1,-1))).toarray().reshape(-1)
-    y = (encoder.transform(np.array([data[match]["Team2"]]).reshape(1,-1))).toarray().reshape(-1)
-    z = (encoder.transform(np.array([data[match]["Winner"]]).reshape(1,-1))).toarray().reshape(-1)
+T1 = False
+T2 = False
+x = None
+y = None
+z = None
 
-    if np.array_equal(x,z):
-        z = 1
-    else:
-        z = 0
+for row in datalist:
+    if row["participantid"] == "100":
+        x = encoder.transform(np.array(row["teamname"]).reshape(1,1)).toarray()
+        if row["result"] == "1":
+            z = 1
+        else:
+            z = 0
+        T1 = True
 
-    numpyarray = np.concatenate((x, y) ,dtype=np.float32)
-    teamsdata[loopnum] = torch.from_numpy(numpyarray)
-    resultsdata[loopnum] = z
-    loopnum += 1
+    elif row["participantid"] == "200":
+        y = encoder.transform(np.array(row["teamname"]).reshape(1,1)).toarray()
+        T2 = True
+
+    if T1 == True and T2 == True:
+        numpyarray = np.concatenate((x, y), axis=None, dtype=np.float32)
+        teamsdata[loopnum] = torch.from_numpy(numpyarray)
+        resultsdata[loopnum] = z
+        loopnum += 1
+        T1 = False
+        T2 = False
 
 
 X_data = teamsdata
 y_data = resultsdata
 
-X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.2, shuffle=False)
 
-team1 = X_train[:,:3]
-team2 = X_train[:,3:]
+train_dataset = TensorDataset(X_train, y_train)
+test_dataset = TensorDataset(X_test, y_test)
 
-X_train_flipped = torch.cat((team2, team1), dim=1)
-y_train_flipped = (1 - y_train).float()
-
-X_train = torch.cat((X_train, X_train_flipped), dim=0)
-y_train = torch.cat((y_train, y_train_flipped), dim=0)
+train_loader = DataLoader(train_dataset, batch_size=128, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
 
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
         self.passthrough = nn.Sequential(
-            nn.Linear(6, 10),
+            nn.Linear(twoteamfeats, 256),
             nn.ReLU(),
-            nn.Linear(10, 10),
+            nn.Dropout(0.1),
+            nn.Linear(256, 128),
             nn.ReLU(),
-            nn.Linear(10, 10),
+            nn.Dropout(0.05),
+            nn.Linear(128, 32),
             nn.ReLU(),
-            nn.Linear(10, 1)
+            nn.Linear(32, 1)
         )
 
     def forward(self, x):
@@ -81,7 +99,7 @@ model1 = Net()
 
 loss_fn = nn.BCEWithLogitsLoss()
 
-optimizer = torch.optim.Adam(model1.parameters(), lr=0.001)
+optimizer = torch.optim.AdamW(model1.parameters(), lr=0.0005, weight_decay=0.0005)
 
 def accuracy_fn(y_true, y_pred):
     correct = torch.eq(y_true, y_pred).sum().item()
@@ -89,8 +107,7 @@ def accuracy_fn(y_true, y_pred):
     accuracy = correct / len(y_pred) * 100
     return accuracy
 
-epochs = 400
-
+epochs = 100
 trainloss = []
 testloss = []
 trainaccuracy = []
@@ -99,38 +116,56 @@ epochcount = []
 
 for epoch in range(epochs):
     model1.train()
+    train_batch_preds = []
+    train_batch_labels = []
+    train_batch_loss = 0
 
-    y_logits = model1(X_train)
+    for X_batch, y_batch in train_loader:
+        y_logits = model1(X_batch)
+        loss = loss_fn(y_logits, y_batch)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-    loss = loss_fn(y_logits, y_train)
+        train_batch_loss += loss.item() * len(y_batch)
+        train_batch_preds.append(torch.sigmoid(y_logits))
+        train_batch_labels.append(y_batch)
 
-    train_acc = accuracy_fn(y_true=y_train, y_pred=torch.round(torch.sigmoid(y_logits)))
+    train_epoch_preds = torch.cat(train_batch_preds)
+    train_epoch_labels = torch.cat(train_batch_labels)
+    train_epoch_loss = train_batch_loss / len(train_loader.dataset)
+    train_epoch_acc = accuracy_fn(y_true=train_epoch_labels, y_pred=torch.round(train_epoch_preds))
 
-    trainloss.append(loss)
-    trainaccuracy.append(train_acc)
-
-    optimizer.zero_grad()
-
-    loss.backward()
-
-    optimizer.step()
+    trainloss.append(train_epoch_loss)
+    trainaccuracy.append(train_epoch_acc)
 
     model1.eval()
-    with torch.inference_mode():
+    with (torch.inference_mode()):
+        test_batch_preds = []
+        test_batch_labels = []
+        test_batch_loss = 0
 
-        test_logits = model1(X_test)
-        test_preds = torch.round(torch.sigmoid(test_logits))
+        for X_batch, y_batch in test_loader:
+            test_logits = model1(X_batch)
+            test_loss = loss_fn(test_logits, y_batch)
 
-        test_loss = loss_fn(test_logits, y_test)
-        test_acc = accuracy_fn(y_true=y_test, y_pred=test_preds)
+            test_batch_loss += test_loss.item() * len(y_batch)
+            test_batch_labels.append(y_batch)
+            test_batch_preds.append(torch.sigmoid(test_logits))
 
-        testloss.append(test_loss)
-        testaccuracy.append(test_acc)
+
+        test_epoch_preds = torch.cat(test_batch_preds)
+        test_epoch_labels = torch.cat(test_batch_labels)
+        test_epoch_loss = test_batch_loss / len(test_loader.dataset)
+        test_epoch_acc = accuracy_fn(y_true=test_epoch_labels, y_pred=torch.round(test_epoch_preds))
+
+        testloss.append(test_epoch_loss)
+        testaccuracy.append(test_epoch_acc)
         epochcount.append(epoch)
 
-        if epoch == 399:
-            ax[0].plot(epochcount, [v.detach().numpy() for v in trainloss], label="Training Loss")
-            ax[0].plot(epochcount, [v.detach().numpy() for v in testloss], label="Test Loss")
+        if epoch == 99:
+            ax[0].plot(epochcount, [v for v in trainloss], label="Training Loss")
+            ax[0].plot(epochcount, [v for v in testloss], label="Test Loss")
             ax[0].set_title("Training and Test Loss")
             ax[0].set_ylabel("Loss")
             ax[0].set_xlabel("Epoch")
@@ -143,16 +178,19 @@ for epoch in range(epochs):
             ax[1].set_xlabel("Epoch")
             ax[1].legend(prop={'size': 14})
 
-            match1, match2, match3 = torch.split(X_test, 1, dim=0)
-            test_match1 = encoder.inverse_transform(match1.reshape(2, 3)).reshape(2)
-            test_match2 = encoder.inverse_transform(match2.reshape(2, 3)).reshape(2)
-            test_match3 = encoder.inverse_transform(match3.reshape(2, 3)).reshape(2)
+
+            match1 = torch.cat(torch.split(X_test[0], 1, dim=0))
+            match2 = torch.cat(torch.split(X_test[1], 1, dim=0))
+            match3 = torch.cat(torch.split(X_test[2], 1, dim=0))
+            test_match1 = encoder.inverse_transform(match1.view(2, -1)).reshape(2)
+            test_match2 = encoder.inverse_transform(match2.view(2, -1)).reshape(2)
+            test_match3 = encoder.inverse_transform(match3.view(2, -1)).reshape(2)
 
             test_match1_string = f"{test_match1[0]} vs {test_match1[1]}"
             test_match2_string = f"{test_match2[0]} vs {test_match2[1]}"
             test_match3_string = f"{test_match3[0]} vs {test_match3[1]}"
 
-            test_graph_results = torch.sigmoid(test_logits).transpose(0,1).squeeze()
+            test_graph_results = torch.sigmoid(torch.cat((test_logits[0], test_logits[1], test_logits[2]))) #.transpose(0,1).squeeze()
 
             ax[2].bar([test_match1_string, test_match2_string, test_match3_string], test_graph_results)
             ax[2].set_title("Test Matches And Predictions After Training")
@@ -161,3 +199,10 @@ for epoch in range(epochs):
             ax[2].set_xlabel("Test Matches")
 
             plt.show()
+
+MODEL_PATH = Path("models")
+MODEL_PATH.mkdir(parents=True, exist_ok=True)
+
+MODEL_NAME = "01_Saved_Net_Model"
+MODEL_SAVE_PATH = MODEL_PATH / MODEL_NAME
+torch.save(obj=model1.state_dict(), f=MODEL_SAVE_PATH)
